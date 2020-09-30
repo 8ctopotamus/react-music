276a2bbcf7b113c0ed759715899aebfe0837f71a
spiano <lafleurpiano@gmail.com>
2020-09-29 19:53:20 -0500

    created pad and synths

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { AppContextProvider } from './context';
 import Layout from './components/layout';
 import ModeSwitch from './components/mode-switch'
+import Synth from './components/synth';
 
 function App() {
 
   return (
       <AppContextProvider>
           <Layout>
-<ModeSwitch/>
+            <ModeSwitch/>
+            <Synth/>
           </Layout>
       </AppContextProvider>
   );
