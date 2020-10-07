6ead74fd668227045748adaf1d6f0dd161342ffb
spiano <lafleurpiano@gmail.com>
2020-10-06 21:33:51 -0500

    made files for chords in context and synth

---
+++ src/context/chords.js
@@ -0,0 +1,12 @@
+export default {
+    majorChord: [
+        {
+            name: 'C Major',
+            letter: 'z',
+            notes: ['C3', 'E3', 'G3'],
+            color: '#922400',
+        },
+    ],
+    minorChord: [],
+    seventhChord: []
+}
